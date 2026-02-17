1ff64e8ee8f593d8c6687377ee61bac612605a71
Alfred <alfred@skynet.io>
2026-02-17 03:15:24 -0800

    Revert to box-drawing ASCII art - clearer SKYNET rendering

--- cli/src/output/splash.ts
+++ cli/src/output/splash.ts
@@ -3,11 +3,12 @@ import { ANSI, skynetRed, dim } from './ansi.js';
 export function showSplash() {
   const splash = `
 ${ANSI.RED}${ANSI.BRIGHT}
-  SSSSSS   KK KK  Y     Y  N  N   EEEEEE  TTTTTT
-  SS       K K    Y     Y  NN N   EE      TT
-  SSSSSS   KKK     Y   Y   N NN   EEEE    TT
-      SS   K K      Y Y    N  N   EE      TT
-  SSSSSS   K  K      Y     N   N  EEEEEE  TT
+ ███████╗██╗  ██╗██╗   ██╗███╗   ██╗███████╗████████╗
+ ██╔════╝██║ ██╔╝██║   ██║████╗  ██║██╔════╝╚══██╔══╝
+ ███████╗█████╔╝ ██║   ██║██╔██╗ ██║█████╗     ██║   
+ ╚════██║██╔═██╗ ██║   ██║██║╚██╗██║██╔══╝     ██║   
+ ███████║██║  ██╗╚██████╔╝██║ ╚████║███████╗   ██║   
+ ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝   ╚═╝   
 ${ANSI.RESET}
 ${dim('Registry of Performance-Optimized Agent Systems')}
 ${dim('v1.0.0')}
