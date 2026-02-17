95020b17fd6b02c709bc733f51f2c8b3ba3cc1fa
Alfred <alfred@skynet.io>
2026-02-17 03:42:13 -0800

    Final splash redesign - clean & professional for viral videos
    Changed to simple, elegant approach:
    - Massive red border (full width)
    - Clean 'S  K  Y  N  E  T' in center (properly spaced)
    - Tagline centered below
    - Professional, eye-catching, zero rendering artifacts
    - Perfect for screen recording & video demos
    
    This is the one.

--- cli/src/output/splash.ts
+++ cli/src/output/splash.ts
@@ -3,22 +3,16 @@ import { ANSI, skynetRed, dim } from './ansi.js';
 export function showSplash() {
   const splash = `
 ${ANSI.RED}${ANSI.BRIGHT}
-
-████████████████████████████████████████████████████████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-████                                                                          ████
-████                                                                          ████
-████          SSSSSSS  KK    KK  YYYY    YYYY  NNN   NNN  EEEEEEE  TTTT    ████
-████          SS       KK   KK   YYYY    YYYY  NNNN  NNN  EE       TTTT    ████
-████          SSSSS    KKKKKK    YYYY    YYYY  NN NN NNN  EEEEE    TTTT    ████
-████          SS       KK  KK     YYYY  YYYY   NN  NNNNN  EE       TTTT    ████
-████          SSSSSSS  KK   KK      YYYY       NN   NNNN  EEEEEEE  TTTT    ████
-████                                                                          ████
-████          Registry of Performance-Optimized Agent Systems               ████
-████                                                                          ████
-████████████████████████████████████████████████████████████████████████████████
-████████████████████████████████████████████████████████████████████████████████
-
+█████████████████████████████████████████████████████████████████████████████████
+█████████████████████████████████████████████████████████████████████████████████
+█                                                                               █
+█                                                                               █
+█                  S  K  Y  N  E  T                                            █
+█                                                                               █
+█              Registry of Performance-Optimized Agent Systems                █
+█                                                                               █
+█████████████████████████████████████████████████████████████████████████████████
+█████████████████████████████████████████████████████████████████████████████████
 ${ANSI.RESET}
 ${dim('v1.0.0  •  Live on Vercel  •  API Ready')}
 
